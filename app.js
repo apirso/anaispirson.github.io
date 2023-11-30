cf4f72c820c130e3e0b914219449f20990e0593c
AnaisP <anaispirson@ymail.com>
2023-11-30 22:53:41 +0100

    advent

---
+++ app.js
@@ -0,0 +1,232 @@
+let appCal = document.querySelector('#app-calendar')
+let advType= document.querySelector('#advType') //defines what genre the cal will show
+let genre = checkCookie('advType');
+let day;
+let elements;
+let currentDate = new Date();
+// let curDay = 7;
+let curDay = currentDate.getDate()
+let curMonth = currentDate.getMonth()+1;
+let advMonth = 12; //testing for nov
+
+pageCreation()
+
+//makes the calander board with closed doors and sets a click element on all doors
+function pageCreation(){
+for (let day = 1; day <= 24; day +=1){
+  appCal.insertAdjacentHTML("beforeend",`<div class ='backDoor'><div class ='tapeOff'></div>
+  <img class='movieImg' id='movieImg-${day}' src="${horror[day-1].image}"/><div class ='day'>
+  <div id='day${day}'><div class ='tapeOn'></div>${day}</div></div></div>`)
+  window.addEventListener('load',loadFilm) //loadFilm will check if doors have previously been selected
+}
+
+day = document.querySelectorAll('.day')
+day.forEach(function (i) {
+  i.addEventListener('click', function(){
+    
+      checkDoor(i)
+  
+      
+}
+  );
+});
+
+//opens a door when clicked on
+ for (let i = 0; i < day.length; i++) {
+  day[i].addEventListener("click", function() {
+    if( curMonth == advMonth && curDay >= (i+1) ) {
+    day[i].classList.toggle("doorOpen");
+    } else{
+      alert("HO HO HO! Don't be naughty now! It's too early...Santa would not appreciate this!")
+    }
+     });
+ }
+
+}
+
+  //function happens on pageLoad and checks what genre to show
+  function loadFilm () {
+    console.log('page loaded')
+
+    
+    //make board based on a user's selection. if the selection has not been chosen or the it's the user's first time, default and setcookie to classic
+    if(genre=='horror'){ 
+        advType.selectedIndex =1
+    
+    }
+    else{
+
+        // setCookie('advType','classic',30)
+        setCookie('advType','horror',30)
+    }
+ let allDoors= checkCounter()  //this function will return either adventDaysCounter which will be either an empty array or array full of previously checked days 
+showOpenDoors(allDoors)
+popUpInfo()
+}
+
+function popUpInfo(){
+  elements= document.querySelectorAll('.movieImg');
+  let clickEvent = (e) => {
+
+    let str=e.srcElement.attributes.id.nodeValue
+   str = str.replace('movieImg-','')
+
+
+let popup= document.getElementById('popup-container')
+  let popupImg=popup.children[0].childNodes[3].childNodes[1].childNodes[1]
+popupImg.src=e.target.currentSrc;
+let popupTitle = popup.children[0].childNodes[3].children[1].children[0].innerText=`${horror[str-1].movie}`
+let popupText = popup.children[0].childNodes[3].children[1].children[1].innerText=`${horror[str-1].description}`;
+let btnLink=popup.children[0].childNodes[3].children[1].childNodes[5].href=`${horror[str-1].link}`;
+
+    popup.style.visibility='visible'
+
+}
+
+
+elements.forEach((item) => {
+item.addEventListener('click', clickEvent)
+});
+let close = document.querySelector('#closeBtn');
+let closeEvent = () => {
+document.getElementById('popup-container').style.visibility='hidden'
+}
+close.addEventListener('click', closeEvent)
+}
+
+//checkCounter will look at the cookies to see if the user has previously opened a day. If yes, then the funciton will return convertCounter
+  function checkCounter(){
+    if(checkCookie('adventDays')){
+    // console.log('days have been created')
+    return convertCounter()
+  }
+else{
+  // console.log('new counter will need to be created')
+  return createCounter()
+}
+}
+;
+
+function convertCounter(){
+// console.log('check cookie and convert into array')
+let adventDaysCounter = checkCookie('adventDays');
+adventDaysCounter = adventDaysCounter.split('|').map(Number);
+return adventDaysCounter;
+}
+
+function createCounter(){
+  let adventDaysCounter =[] 
+  return adventDaysCounter; //should i just return []
+}
+
+function showOpenDoors(allDoors){
+  //for all doors that have previously been opened, show image
+let i = 0;
+while (i<allDoors.length) {
+  loop(i);
+  i++
+}
+
+function loop(i){
+  setTimeout(function(){
+    document.getElementById(`day${[allDoors[i]]}`).parentElement.classList.toggle("doorOpen");
+  }, 200 * i)
+}
+
+
+}
+
+
+  //getCookie function based on advType
+  function checkCookie(cname){
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+      }
+
+      //setCookie
+      function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
+
+
+
+
+
+function updateCounter(dayNo){ //specify wht counter is for
+
+  if( curMonth == advMonth && curDay >= (dayNo) ) {
+
+  let adventDaysCounter= checkCounter();
+    if(!adventDaysCounter.includes(dayNo)){
+      
+    adventDaysCounter.push(dayNo);
+    adventDaysCounter.sort(function(a, b){return a-b});
+    // console.log(adventDaysCounter)
+    updateCookie(adventDaysCounter)
+}
+else{
+    // console.log('value is already in there')
+}
+}}
+      function updateCookie(adventDaysCounter){
+        let updated = adventDaysCounter.join('|')
+        setCookie('adventDays', updated, 30)
+      }
+  
+//change of dropdown option
+advType.addEventListener('change',()=>{
+    if (advType.selectedIndex != 0){
+        // console.log('updated to horror')
+        setCookie('advType','horror',30)
+
+    }
+         else{
+        // console.log('updated to classical')
+        alert('classical is not ready...please watch horror')
+        // setCookie('advType','classic',30)
+        setCookie('advType','horror',30)
+         }
+        
+     })
+     
+function checkDoor(day){
+let dayNo = parseInt(day.innerText)
+
+if(checkCookie('advType') =='horror'){
+    updateCounter(dayNo)
+    } else{
+        // console.log('Show data for clas:' + classic[dayNo -1].movie)
+    }
+
+}
+
+
+// www.cssscript.com/winter-snow-animation/§
+const snowflake = new SnowflakeJs(frames=25, count=50, lifetime=100, maxSpeed=3, maxSize=25);
+snowflake.init();
+
+
+function createCard(){
+
+
+}
+// appCal.insertAdjacentHTML("beforeend",`<div class ='backDoor'><div class ='tapeOff'></div>
+//   <img class='movieImg' src="https://m.media-amazon.com/images/M/MV5BNmE5NWQyYjktMmMzYi00YjY2LWFmNTUtMWQyMzcwNWM2NWYzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL75_UX380_CR0,0,380,562_.jpg"/><div class ='day'>
+//   <div id='day${day}'><div class ='tapeOn'></div>${day}</div></div></div>`)
+
+
+
---
+++ films.js
@@ -0,0 +1,452 @@
+let classic = [{
+    day:1,
+    movie:'Home Alone (1990)',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:2,
+    movie:'TEST TEST TEST - CLASSIC -2',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:3,
+    movie:'TEST TEST TEST - CLASSIC -3',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:4,
+    movie:'TEST TEST TEST - CLASSIC -4',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:5,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:6,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:7,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:8,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:9,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:10,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:11,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:12,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:13,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:14,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:15,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:16,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:17,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:18,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:19,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:20,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:21,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:22,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:23,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+},
+{
+    day:24,
+    movie:'TEST TEST TEST - CLASSIC -5',
+    image:'HTTPS://',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https:/',
+    rating: 0,
+    genre:''
+}
+]
+
+
+
+let horror = [{
+    day: 1,
+    movie:'Better Watch Out (2016)',
+    image:'https://m.media-amazon.com/images/M/MV5BNmE5NWQyYjktMmMzYi00YjY2LWFmNTUtMWQyMzcwNWM2NWYzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL75_UX380_CR0,0,380,562_.jpg',
+    description:'A babysitter must defend a 12 yr old boy from home intruders.',
+    link: 'https://www.imdb.com/title/tt4443658/',
+    rating: 6.5,
+    genre:'Comedy/Horror/Thriller'
+},
+{
+    day:2,
+    movie:'Grinch: The Mean One (2022)',
+    image:'https://m.media-amazon.com/images/M/MV5BMTUxODA1NDMtNWM0YS00YzQ2LTlmMzgtNGExMTQ4NTE3ZjU2XkEyXkFqcGdeQXVyOTQzOTE5NTY@._V1_FMjpg_UY7200_.jpg',
+    description:'Cindys parents are murdered by a bloodthirsty green figure in a Santa suit.',
+    link: 'https://www.imdb.com/title/tt20853816/',
+    rating: 3.8,
+    genre:'Comedy/Horror'
+},
+{
+    day:3,
+    movie:'Jack Frost (1997)',
+    image:'https://m.media-amazon.com/images/M/MV5BYjE4ZDAxNmItODBjMy00YjVlLTkwMDMtZjYxZWY0YmI4ZDczXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_QL75_UY562_CR14,0,380,562_.jpg',
+    description:'After an accident leaves murderer Jack Frost dead. he returns as a murderous snowman for revenge.',
+    link: 'https://m.media-amazon.com/images/M/MV5BYjE4ZDAxNmItODBjMy00YjVlLTkwMDMtZjYxZWY0YmI4ZDczXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_QL75_UY562_CR14,0,380,562_.jpg',
+    rating: 4.7,
+    genre:'Comedy/Fantasy/Horror'
+},
+{
+    day:4,
+    movie:'Silent Night (2021)',
+    image:'https://m.media-amazon.com/images/M/MV5BZDE1NWVjODItYTM0MS00MGJjLTkzMjctNTJjZjkwZDExYzNhXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UY4096_.jpg',
+    description:'A family of 5 welcome friends for a perfect Christmas gathering..Perfect except for one thing: everyone is going to die.',
+    link: 'https://www.imdb.com/title/tt11628854/',
+    rating: 5.7,
+    genre:'Comedy/Horror'
+},
+{
+    day:5,
+    movie:'Gremlins (1984)',
+    image:'https://m.media-amazon.com/images/M/MV5BZDNlZWYyNTctMDk0Ni00MGI2LWFlOTQtY2M0YWZkZDY4ZjJiXkEyXkFqcGdeQXVyNjc5NjEzNA@@._V1_FMjpg_UY3000_.jpg',
+    description:'A young man breaks 3 rules concerning his new pet and unleashes monsters on a town.',
+    link: 'https://www.imdb.com/title/tt0087363/',
+    rating: 7.3,
+    genre:'Comedy/Fantasy/Horror'
+},
+{
+    day:6,
+    movie:'A Christmas Horror Story (2015)',
+    image:'https://m.media-amazon.com/images/M/MV5BMTQ0OTk4MTg1MF5BMl5BanBnXkFtZTgwODUwMzI4NjE@._V1_FMjpg_UX325_.jpg',
+    description:'Interwoven stories that take place on Christmas Eve.',
+    link: 'https://www.imdb.com/title/tt3688406/',
+    rating: 5.8,
+    genre:'Fantasy/Horror/Mystery'
+},
+{
+    day:7,
+    movie:'P2 (2007)',
+    image:'https://m.media-amazon.com/images/M/MV5BMTk2Nzk5ODAxMF5BMl5BanBnXkFtZTcwNzA0MTU1MQ@@._V1_QL75_UX380_CR0,1,380,562_.jpg',
+    description:'A businesswoman is pursued by a psychopath after being locked in a parking lot on Christmas Eve.',
+    link: 'https://www.imdb.com/title/tt0804516/',
+    rating: 5.9,
+    genre:'Crime/Horror/Thriller'
+},
+{
+    day:8,
+    movie:'Sint (2010)',
+    image:'https://m.media-amazon.com/images/M/MV5BMjA5ODgyMjA5NF5BMl5BanBnXkFtZTcwNjY3Mzk5Mw@@._V1_FMjpg_UY2717_.jpg',
+    description:'A Dutch horror where St. Nicholas is a bishop who kidnaps and murders children when there is a full moon on December 5.',
+    link: 'https://www.imdb.com/title/tt1167675/',
+    rating: 5.6,
+    genre:'Comedy/Fantasy/Horror'
+},
+{
+    day:9,
+    movie:'The Gingerdead Man (2005)',
+    image:'https://m.media-amazon.com/images/M/MV5BMTgyNTUwOTUxN15BMl5BanBnXkFtZTgwNjE0NzgwMzE@._V1_FMjpg_UX341_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:'An evil Gingerbread man comes to life with the soul of a convicted killer to get revenge on the girl who sent him to the electric chair.',
+    link: 'https://www.imdb.com/title/tt0364376/',
+    rating: 3.4,
+    genre:' Comedy/Fantasy/Horror'
+},
+{
+    day:10,
+    movie:"Santa: Santa\'s Slay (2005)",
+    image:'https://m.media-amazon.com/images/M/MV5BMTU1NTY0MzMyMl5BMl5BanBnXkFtZTcwNzU0OTEzMQ@@._V1_QL75_UY562_CR4,0,380,562_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:'Santa is a demon who lost a bet with an angel, so he becomes the giver of toys and happiness...now all bets are off',
+    link: 'https://www.imdb.com/title/tt0393685/',
+    rating: 5.3,
+    genre:'Comedy/Fantasy/Horror'
+},
+{
+    day:11,
+    movie:'Adult Swim Yule Log (2022)',
+    image:'https://m.media-amazon.com/images/M/MV5BMmRkMGJhZjEtNThlZS00ZjU2LThiN2EtMWMzZWI3OTMyMTIyXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:'A dinner devolves into a nightmare, as a couple and podcasters spend the night in a double-booked cabin that is occupied by evil and murderous forces.',
+    link: 'https://www.imdb.com/title/tt24248198/',
+    rating: 6.3,
+    genre:'Comedy/Horror'
+},
+{
+    day:12,
+    movie:'Black Christmas (2006)',
+    image:'https://m.media-amazon.com/images/M/MV5BMTY0NDQzODE2OV5BMl5BanBnXkFtZTgwNjQ4ODE3MDI@._V1_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:'On Christmas Eve, an escaped killer returns to their childhood home, which is now a sorority house',
+    link: 'https://www.imdb.com/title/tt0454082/',
+    rating: 4.7,
+    genre:''
+},
+{
+    day:13,
+    movie:'Violent Night (2022)',
+    image:'https://m.media-amazon.com/images/M/MV5BYzg2NWNhOWItYjA3Yi00MzhhLTg4ZmItYzM3ZTIwN2U0ZGQ5XkEyXkFqcGdeQXVyMzEyMDQzNzY@._V1_QL75_UY562_CR35,0,380,562_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:'A group of people attack a wealthy family estate and Santa Claus must save the day.',
+    link: 'https://www.imdb.com/title/tt12003946/',
+    rating: 6.7,
+    genre:'Action/Comedy/Thriller'
+},
+{
+    day:14,
+    movie:'The Nightmare Before Christmas (1993)',
+    image:'https://m.media-amazon.com/images/M/MV5BNWE4OTNiM2ItMjY4Ni00ZTViLWFiZmEtZGEyNGY2ZmNlMzIyXkEyXkFqcGdeQXVyMDU5NDcxNw@@._V1_FMjpg_UX1000_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:'Jack Skellington - king of Halloween Town, discovers Christmas Town and attempts to bring Christmas home',
+    link: 'https://www.imdb.com/title/tt0107688/',
+    rating: 7.9,
+    genre:'Animation/Fantasy/Family'
+},
+{
+    day:15,
+    movie:'The Day of the Beast (1995)',
+    image:'https://m.media-amazon.com/images/M/MV5BZWRhMWI2NWUtMTQxNC00MzNmLTk4NWEtMDc3ZDBjNjdkODhkXkEyXkFqcGdeQXVyMTA0MjU0Ng@@._V1_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:'Spanish film - Bent on committing as many sins as possible to avert the birth of the beast, a Catholic priest teams up with a Black Metal aficionado and an Italian connoisseur of the occult. Now, he must become an unrelenting sinner. Is there still hope?',
+    link: 'https://www.imdb.com/title/tt0112922/',
+    rating: 7.3,
+    genre:'Action/Comedy/Crime'
+},
+{
+    day:16,
+    movie:'The Advent Calendar (2021)',
+    image:'https://m.media-amazon.com/images/M/MV5BZDc5NjE5ZGQtOWU1MS00MmVkLTg2MWMtNzBjNTVhMTIxMDZlXkEyXkFqcGdeQXVyMTAwMzM3NDI3._V1_FMjpg_UX793_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:"A French horror where parapelegic Eva gets a 'unique' advent calendar",
+    link: 'https://www.imdb.com/title/tt12496706/',
+    rating: 6.3,
+    genre:'Horror/Thriller'
+},
+{
+    day:17,
+    movie:'Christmas Bloody Christmas (2022)',
+    image:'https://m.media-amazon.com/images/M/MV5BOTRmMDc4ZDAtOTJiNS00YTQ4LWJkM2MtMTQxMDEwNzMyN2MwXkEyXkFqcGdeQXVyMzQwMTY2Nzk@._V1_FMjpg_UX1080_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:"A robotic Santa in a toy store goes haywire and begins a rampant killing spree through her small town, ruining Tori\'s party.",
+    link: 'https://www.imdb.com/title/tt22042742/',
+    rating: 5.1,
+    genre:'Horror'
+},
+{
+    day:18,
+    movie:'The Lodge (2019)',
+    image:'https://m.media-amazon.com/images/M/MV5BNDQ2YWVmZjEtMTE1ZS00MDk1LTg5ODUtMDVlODk0OTM2ODMwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_QL75_UX380_CR0,0,380,562_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:"A stepmom is snowed in with her partner\'s children at a remote holiday village and frightening events take place.",
+    link: 'https://www.imdb.com/title/tt7347846/',
+    rating: 6.2,
+    genre:'Drama/Horror/Mystery'
+},
+{
+    day:19,
+    movie:'Silent Night, Deadly Night (1984)',
+    image:'https://m.media-amazon.com/images/M/MV5BMWJhMWQ5M2QtMTNmNC00NzBjLWIwN2UtYzBhMWM5OTU4ZDhhXkEyXkFqcGdeQXVyMTUzMDUzNTI3._V1_FMjpg_UY2265_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:'Little Billy watches his parents getting killed by Santa after being warned by his grandpa that Santa punishes those who are naughty. Now Billy is 18 and out for revenge as Santa himself',
+    link: 'https://www.imdb.com/title/tt0088117/',
+    rating: 5.8,
+    genre:'Horror/Thriller'
+},
+{
+    day:20,
+    movie:'Killer Christmas Elves (2021)',
+    image:'https://m.media-amazon.com/images/M/MV5BNGJhNWM2NjctN2QyYi00NzgzLTk0MmEtZDNkNDA2MzBjMDVhXkEyXkFqcGdeQXVyNTYwNjIwNQ@@._V1_QL75_UX190_CR0,0,190,281_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:'After a witch curses a toy factory, a father and his son spend Christmas Eve fighting malevolently elves.',
+    link: 'https://www.imdb.com/title/tt15088982/',
+    rating: 6.3,
+    genre:'Action/Comedy/Horror'
+},
+{
+    day:21,
+    movie:'Dead End (2003)',
+    image:'https://m.media-amazon.com/images/M/MV5BNTM2YWZhMjktMDk5My00YzMxLTk5ZDAtNmM0NTkyMDZmMWE2XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:'On his way to his in-laws for Christmas, Frank takes a shortcut which ends up being the biggest mistake of his life.',
+    link: 'https://www.imdb.com/title/tt0308152/',
+    rating: 6.5,
+    genre:'Adventure/Horror/Mystery'
+},
+{
+    day:22,
+    movie:'Krampus (2015)',
+    image:'https://m.media-amazon.com/images/M/MV5BMjk0MjMzMTI3NV5BMl5BanBnXkFtZTgwODEyODkxNzE@._V1_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:'A boy who has a bad Christmas accidentally summons a festive demon to his family home.',
+    link: 'https://www.imdb.com/title/tt3850590/',
+    rating: 6.2,
+    genre:'Comedy/Fantasy/Horror'
+},
+{
+    day:23,
+    movie:'Rare Exports: A Christmas Tale (2010)',
+    image:'https://m.media-amazon.com/images/M/MV5BNzM0NzYxOTk0Nl5BMl5BanBnXkFtZTcwMzI1NzQwNA@@._V1_.jpg',
+    location:'WHERE TO FIND - Horror',
+    description:"On Christmas eve in northern Finland, an 'archeological' dig finds the real Santa Claus. But this Santa isn't the one you want coming to town.",
+    link: 'https://www.imdb.com/title/tt1401143/',
+    rating: 6.6,
+    genre:'Adventure/Horror/Mystery'
+},
+{
+    day:24,
+    movie:'Mercy Christmas (2017)',
+    image:'https://m.media-amazon.com/images/M/MV5BODE2NDFjZGMtNDI2Zi00NjRmLTgzNWMtOWY0YmNlY2E0NmQ5XkEyXkFqcGdeQXVyNjc3Njc0Mzg@._V1_.jpg',
+    location:"When Michael meets the perfect woman, he believes that Christmas came early...until she invites him to her family's holiday celebration and he realizes that HE is Christmas dinner.",
+    link: 'https://www.imdb.com/title/tt4906164/',
+    rating: 5.3,
+    genre:'Action/Comedy/Horror'
+}
+]
